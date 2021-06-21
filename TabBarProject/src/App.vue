95f776e0e50324ef70e70737a6a9ec207e89cb3e
liulei-root <952413829@qq.com>
2021-06-21 17:26:47 +0800

    tab_bar_project

---
+++ TabBarProject/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+import TabBar from './components'
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  @import "./assets/css/base.css";
+
+</style>
