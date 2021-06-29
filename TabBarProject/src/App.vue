2280ebf56e7563ad97ac473b80ebdcb6936479c4
liulei-root <952413829@qq.com>
2021-06-29 14:26:10 +0800

    commit setting.json

--- TabBarProject/src/App.vue
+++ TabBarProject/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
-    
+    <tab-bar></tab-bar>
   </div>
 </template>
 
 <script>
-import TabBar from './components'
+import TabBar from './components/tabbar/TabBar.vue'
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    TabBar
+  }
 }
 </script>
 
--- TabBarProject/src/components/tabbar/TabBar.vue
+++ TabBarProject/src/components/tabbar/TabBar.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="tab-bar">
-      <div class="tab-bar-item">首页</div>
+      <div class="tab-bar-item">
+        <img src="../../assets/img/tabbar/home.svg" alt="">
+        首页
+      </div>
       <div class="tab-bar-item">分类</div>
       <div class="tab-bar-item">购物车</div>
       <div class="tab-bar-item">我的</div>
     </div>
-</template>
+</template>         
 
 <script>
 export default {
